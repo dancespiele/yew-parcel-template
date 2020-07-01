0877fdef7baadf74c9c066b528f83d72447a2c9c
spieljs <spieljs@gmail.com>
2020-07-01 19:21:28 +0200

    update yew_parcel_template to 0.17, yew_router to 0.14 and yew_styles to 0.6.2

--- crate/src/app.rs
+++ crate/src/app.rs
@@ -44,7 +44,7 @@ impl Component for App {
                     navbar_type=Palette::Info
                     navbar_style=Style::Outline
                     fixed=Fixed::Top
-                    branch=html!{<img src="./yew.svg"></img>}
+                    branch=html!{<img src="./yew.svg"/>}
                 >
                     <NavbarContainer>
                             <RouterAnchor<AppRouter> classes="navbar-item" route=AppRouter::RootPath>{"Home"}</RouterAnchor<AppRouter>>
