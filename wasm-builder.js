f39af6cd5f897edae4b0d77b1bb90190927de960
spieljs <spieljs@gmail.com>
2021-05-18 00:39:23 +0200

    update version

--- wasm-builder.js
+++ wasm-builder.js
@@ -1,55 +1,63 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const Bundler = require('parcel-bundler');
-const Path = require('path');
-const chokidar = require('chokidar');
-const dotenv = require('dotenv');
-const app = require('express')();
-const compression = require('compression');
+const { execSync } = require("child_process");
+const fs = require("fs");
+const Bundler = require("parcel-bundler");
+const Path = require("path");
+const chokidar = require("chokidar");
+const dotenv = require("dotenv");
+const app = require("express")();
+const compression = require("compression");
 dotenv.config();
 
-const entryFiles = Path.join(__dirname, 'index.html');
+const entryFiles = Path.join(__dirname, "index.html");
 
 const buildType = process.env.NODE_ENV;
 
 const options = {
-    outDir: './dist', 
-    outFile: 'index.html',
-    publicUrl: '/',
-    watch: true,
-    minify: buildType === 'production',
-  };
+  outDir: "./dist",
+  outFile: "index.html",
+  publicUrl: "/",
+  watch: true,
+  minify: buildType === "production",
+};
 
 const bundler = new Bundler(entryFiles, options);
 
-chokidar.watch(['./crate/src', './crate/Cargo.toml']).on('change', async (event, path) => {
-    console.log(`there are new changes in '${path}'. Start to rebuild rustwasm sources`);
+chokidar
+  .watch(["./crate/src", "./crate/Cargo.toml"])
+  .on("change", async (event, path) => {
+    console.log(
+      `there are new changes in '${path}'. Start to rebuild rustwasm sources`
+    );
 
     bundler.bundle();
 
     bundler.hmr.broadcast({
-        type: 'reload'
+      type: "reload",
     });
+  });
+
+bundler.on("buildStart", () => {
+  let prevtBuildFile = Path.join(__dirname, "./wasm_pack_cmd");
+  if (process.platform === "win32") {
+    prevtBuildFile = Path.join(__dirname, "./wasm_pack_cmd.bat");
+  }
+
+  console.log(`running: ${prevtBuildFile}`);
+  execSync(`${prevtBuildFile} ${buildType === "production" ? "" : "--dev"}`, {
+    stdio: "inherit",
+  });
 });
 
-bundler.on('buildStart', () => {
-    let prevtBuildFile = Path.join(__dirname, './wasm_pack_cmd');
-    if(process.platform == "win32") {
-        prevtBuildFile = Path.join(__dirname, './wasm_pack_cmd.bat');
-    }
-
-    console.log(`running: ${prevtBuildFile}`);
-    execSync(`${prevtBuildFile} ${buildType === 'production' ? '' : '--dev'}`, {stdio: 'inherit'});
-});
-
-if(buildType === 'production') {
-    app.use(compression());    
+if (buildType === "production") {
+  app.use(compression());
 }
 
 app.use(bundler.middleware());
 
 app.listen(process.env.SERVER_ADDRESS || 1234, () => {
-    console.log(`Yew app start in address: ${process.env.SERVER_ADDRESS || 'http://localhost:1234'}`);
+  console.log(
+    `Yew app start in address: ${
+      process.env.SERVER_ADDRESS || "http://localhost:1234"
+    }`
+  );
 });
-
-
