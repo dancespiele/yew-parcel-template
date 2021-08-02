c8b9098af47e39d7197bb876adbd7892251452c5
spieljs <spieljs@gmail.com>
2021-08-03 00:36:46 +0200

    update yew styles version

--- .bin/create-yew-parcel.js
+++ .bin/create-yew-parcel.js
@@ -5,7 +5,7 @@ const fs   = require("fs");
 let folderName = '.';
 
 if (process.argv.length >= 3) {
-  folderName = process.argv[2];   
+  folderName = process.argv[2];
   if (!fs.existsSync(folderName)) {
     fs.mkdirSync(folderName);
   }
@@ -18,7 +18,7 @@ clone.on("close", (code) => {
     handleError("install", code);
   } else {
     console.log("Yew and parcel ready. Installing dependencies ...");
-    
+
     const install = spawn('npm', ['install'], { cwd: folderName });
     install.on("close", (code) => {
       if (code !== 0) {
@@ -26,8 +26,9 @@ clone.on("close", (code) => {
       } else {
         console.log(" Installed dependencies ✅ ");
         console.log(" Mucha locura! Bzzzzzzzzzzzzz")
-        console.log(" If you like Yew Parcel Templa, help us supporting the project:"
-        + "\nhttps://gitcoin.co/grants/1050/yew-parcel-template\nhttps://github.com/sponsors/dancespiele\nhttps://paypal.me/dancespiele?locale.x=en_US");
+        console.log(" If you like Yew Parcel Template, help us supporting the project:"
+        + "\nBAT rewards in case that you use Brave Browser in https://github.com/dancespiele 
+        + "\nUsing this link https://www.binance.com/en/register?ref=DB8EPXF0 to create an account in Binance (get 10% fee back for every trading)");
       }
     });
   }
